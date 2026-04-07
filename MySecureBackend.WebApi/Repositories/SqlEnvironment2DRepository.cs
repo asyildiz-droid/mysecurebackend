875b5255278f7704fe24d8e15de04612e4efcffd
Abdulsamed <as.yildiz@student.avans.nl>
2026-04-07 13:45:46 +0200

    aanpassingen

--- MySecureBackend.WebApi/Repositories/SqlEnvironment2DRepository.cs
+++ MySecureBackend.WebApi/Repositories/SqlEnvironment2DRepository.cs
@@ -1,5 +1,5 @@
 ﻿using Dapper;
-using Microsoft.Data.SqlClient;
+using Npgsql;
 using MySecureBackend.WebApi.Interface;
 using MySecureBackend.WebApi.Models;
 
@@ -16,68 +16,53 @@ namespace MySecureBackend.WebApi.Repositories
 
         public async Task InsertAsync(Environment2D environment2D)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                await sqlConnection.ExecuteAsync("INSERT INTO [Environment2D] (Id, Name, MaxHeight, MaxLength, UserId) VALUES (@Id, @Name, @MaxHeight, @MaxLength, @UserId)", environment2D);
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("INSERT INTO \"Environment2D\" (\"Id\", \"Name\", \"MaxHeight\", \"MaxLength\", \"UserId\") VALUES (@Id, @Name, @MaxHeight, @MaxLength, @UserId)", environment2D);
         }
 
         public async Task<Environment2D?> SelectAsync(Guid id)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QuerySingleOrDefaultAsync<Environment2D>("SELECT * FROM [Environment2D] WHERE Id = @Id", new { id });
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QuerySingleOrDefaultAsync<Environment2D>("SELECT * FROM \"Environment2D\" WHERE \"Id\" = @Id", new { id });
         }
 
         public async Task<IEnumerable<Environment2D>> SelectAsync()
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QueryAsync<Environment2D>("SELECT * FROM [Environment2D]");
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QueryAsync<Environment2D>("SELECT * FROM \"Environment2D\"");
         }
 
         public async Task UpdateAsync(Environment2D environment2D)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                await sqlConnection.ExecuteAsync("UPDATE [Environment2D] SET " +
-                                                 "Name = @Name, " +
-                                                 "MaxHeight = @MaxHeight, " +
-                                                 "MaxLength = @MaxLength, " +
-                                                 "UserId = @UserId " +
-                                                 "WHERE Id = @Id", environment2D);
-
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("UPDATE \"Environment2D\" SET " +
+                                     "\"Name\" = @Name, " +
+                                     "\"MaxHeight\" = @MaxHeight, " +
+                                     "\"MaxLength\" = @MaxLength, " +
+                                     "\"UserId\" = @UserId " +
+                                     "WHERE \"Id\" = @Id", environment2D);
         }
 
         public async Task DeleteAsync(Guid id)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                await sqlConnection.ExecuteAsync("DELETE FROM [Environment2D] WHERE Id = @Id", new { id });
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("DELETE FROM \"Environment2D\" WHERE \"Id\" = @Id", new { id });
         }
 
         public async Task<IEnumerable<Environment2D>> SelectByUserIdAsync(string userId)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QueryAsync<Environment2D>(
-                    "SELECT * FROM [Environment2D] WHERE UserId = @UserId",
-                    new { UserId = userId });
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QueryAsync<Environment2D>(
+                "SELECT * FROM \"Environment2D\" WHERE \"UserId\" = @UserId",
+                new { UserId = userId });
         }
 
         public async Task<Environment2D?> SelectByUserIdAndNameAsync(string userId, string name)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QuerySingleOrDefaultAsync<Environment2D>(
-                    "SELECT * FROM [Environment2D] WHERE UserId = @UserId AND Name = @Name",
-                    new { UserId = userId, Name = name });
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QuerySingleOrDefaultAsync<Environment2D>(
+                "SELECT * FROM \"Environment2D\" WHERE \"UserId\" = @UserId AND \"Name\" = @Name",
+                new { UserId = userId, Name = name });
         }
     }
 }
--- MySecureBackend.WebApi/Repositories/SqlObject2DRepository.cs
+++ MySecureBackend.WebApi/Repositories/SqlObject2DRepository.cs
@@ -1,5 +1,5 @@
 ﻿using Dapper;
-using Microsoft.Data.SqlClient;
+using Npgsql;
 using MySecureBackend.WebApi.Interface;
 using MySecureBackend.WebApi.Models;
 
@@ -16,51 +16,40 @@ namespace MySecureBackend.WebApi.Repositories
 
         public async Task InsertAsync(Object2D object2D)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-               await sqlConnection.ExecuteAsync("INSERT INTO [Object2D] (Id, PrefabId, PositionX, PositionY, ScaleX, ScaleY, RotationZ, SortingLayer) VALUES (@Id, @PrefabId, @PositionX, @PositionY, @ScaleX, @ScaleY, @RotationZ, @SortingLayer)", object2D);
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("INSERT INTO \"Object2D\" (\"Id\", \"PrefabId\", \"PositionX\", \"PositionY\", \"ScaleX\", \"ScaleY\", \"RotationZ\", \"SortingLayer\") VALUES (@Id, @PrefabId, @PositionX, @PositionY, @ScaleX, @ScaleY, @RotationZ, @SortingLayer)", object2D);
         }
 
         public async Task<Object2D?> SelectAsync(Guid id)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QuerySingleOrDefaultAsync<Object2D>("SELECT * FROM [Object2D] WHERE Id = @Id", new { id });   
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QuerySingleOrDefaultAsync<Object2D>("SELECT * FROM \"Object2D\" WHERE \"Id\" = @Id", new { id });
         }
 
         public async Task<IEnumerable<Object2D>> SelectAsync()
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QueryAsync<Object2D>("SELECT * FROM [Object2D]");
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            return await conn.QueryAsync<Object2D>("SELECT * FROM \"Object2D\"");
         }
 
         public async Task UpdateAsync(Object2D object2D)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                await sqlConnection.ExecuteAsync("UPDATE [Object2D] SET " +
-                                                 "PrefabId = @PrefabId, " +
-                                                 "PositionX = @PositionX, " +
-                                                 "PositionY = @PositionY, " +
-                                                 "ScaleX = @ScaleX, " +
-                                                 "ScaleY = @ScaleY, " +
-                                                 "RotationZ = @RotationZ, " +
-                                                 "SortingLayer = @SortingLayer " +
-                                                 "WHERE Id = @Id", object2D);
-
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("UPDATE \"Object2D\" SET " +
+                                     "\"PrefabId\" = @PrefabId, " +
+                                     "\"PositionX\" = @PositionX, " +
+                                     "\"PositionY\" = @PositionY, " +
+                                     "\"ScaleX\" = @ScaleX, " +
+                                     "\"ScaleY\" = @ScaleY, " +
+                                     "\"RotationZ\" = @RotationZ, " +
+                                     "\"SortingLayer\" = @SortingLayer " +
+                                     "WHERE \"Id\" = @Id", object2D);
         }
 
         public async Task DeleteAsync(Guid id)
         {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                await sqlConnection.ExecuteAsync("DELETE FROM [Object2D] WHERE Id = @Id", new { id });
-            }
+            using var conn = new NpgsqlConnection(sqlConnectionString);
+            await conn.ExecuteAsync("DELETE FROM \"Object2D\" WHERE \"Id\" = @Id", new { id });
         }
     }
 }
